00591e3642cb62955e6acd7328b74cd90893b522
Simon Jørgensen <sijo819@student.sdu.dk>
2021-10-15 11:01:48 +0200

    Add react-navigation

---
+++ components/views/save.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function SaveScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Save!</Text>
+    </View>
+  );
+}
+
+
+export default SaveScreen;
