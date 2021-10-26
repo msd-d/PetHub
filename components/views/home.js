f3ddb7a8b2712bd3d14ead069de9ec0218cebbae
Simon Jørgensen <55147377+Joergenson@users.noreply.github.com>
2021-10-26 19:14:02 +0200

    Home initial implementation
    * Add react-native-elements
    
    * Add base implementation of home
    
    * Add linear gradient
    
    * Add colors and fonts
    
    * Update home.js
    
    * Remove unused variables from App.js

--- components/views/home.js
+++ components/views/home.js
@@ -1,13 +1,150 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, FlatList, StyleSheet } from 'react-native';
+import { Chip } from 'react-native-elements';
+import config from '../config';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaskedView from '@react-native-masked-view/masked-view';
+import {textColors, chipColors} from '../colors/home-colors';
+import { ViewPropTypes } from 'react-native';
+
+const apiData = [
+  {
+    id: 1,
+    name: "Daisy",
+    breed: 'Mixed',
+    gender: 'Female',
+    age: 5,
+    image: require("../../images/image1.png"),
+  },
+  {
+    id: 2,
+    name: "Daisy2",
+    breed: 'Mixed',
+    gender: 'Female',
+    age: 5,
+    image: require("../../images/image1.png"),
+  },
+  {
+    id: 3,
+    name: "Gustav",
+    breed: 'Alcholic',
+    gender: 'Female',
+    age: 5,
+    image: require("../../images/image1.png"),
+  },
+];
+
+const GradientText = (props) => {
+  GradientText.propTypes = {
+    style: ViewPropTypes.style,
+  }
+  return (
+    <MaskedView maskElement={<Text {...props} />}>
+      <LinearGradient
+        colors={[ "rgba(232, 193, 171, 1)","rgba(223, 122, 153, 1)"]}
+        start={{ x: -1, y: -1 }}
+        end={{ x: 1, y: 3 }}
+      >
+        <Text {...props} style={[props.style, styles.gradientText]} />
+      </LinearGradient>
+    </MaskedView>
+  );
+};
+
+const renderItem = ({ item }) => (
+  <View style={styles.card}>
+    <Image
+      source={item.image}
+      resizeMode="cover"
+      overflow='hidden'
+      style={styles.image}
+    />
+    <View style={styles.cardContent}>
+      <Text style={styles.name}>{item.name}</Text>
+      <View style={styles.chipBox}>
+        <Chip
+          title={<GradientText style={styles.chipText}>{item.breed}</GradientText>}
+          titleStyle={styles.chipText}
+          buttonStyle={styles.chip}
+        />
+        <Chip
+          title={<GradientText style={styles.chipText}>{item.gender}</GradientText>}
+          titleStyle={styles.chipText}
+          buttonStyle={styles.chip}
+        />
+        <Chip
+          title={<GradientText style={styles.chipText}>{item.age + " years old"}</GradientText>}
+          titleStyle={styles.chipText}
+          buttonStyle={styles.chip}
+        />
+      </View>
+    </View>
+  </View>
+);
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
+    <FlatList
+      data={apiData}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={renderItem}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    display: 'flex',
+    width: config.deviceWidth * 0.9,
+    marginLeft: config.deviceWidth * 0.05,
+    height: config.deviceWidth * 0.9,
+    marginTop: 15,
+    borderRadius: 23
+  },
+  cardContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    bottom: 1,
+    alignSelf: 'center',
+    width: config.deviceWidth * 0.8,
+    height: 100,
+  },
+  name: {
+    fontFamily: 'Inter_700Bold',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 22,
+    color: textColors.cardName,
+    marginBottom: 10,
+  },
+  chip: {
+    backgroundColor: chipColors.chipBackground,
+    marginRight: 10,
+    borderRadius: 8
+  },
+  chipText: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 12,
+    height: 20
+  },
+  chipBox: { 
+    display: 'flex', 
+    flexDirection: 'row' 
+  },
+  image: { 
+    width: undefined, 
+    height: '100%', 
+    overflow: 'hidden', 
+    borderRadius: 23, 
+  },
+  gradientText: { 
+    opacity: 0,
+  }
+})
 
 
 export default HomeScreen;
