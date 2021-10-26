5aa4c9c6d0ee58cf7a945faa516f85dda646516b
Simon Jørgensen <sijo819@student.sdu.dk>
2021-10-26 18:40:28 +0200

    Remove unused variables from App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Settings, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from 'components/views/home';
