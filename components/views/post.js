6fad7dd52ed85ae9896edbc9f51da6f945888081
Simon Jørgensen <sijo819@student.sdu.dk>
2021-10-27 19:42:27 +0200

    Update post.js

--- components/views/post.js
+++ components/views/post.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Text, StyleSheet, Platform } from 'react-native';
+import { View, Image, Text, StyleSheet, Platform, ScrollView } from 'react-native';
 import { Button, ButtonGroup } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native';
 import config from '../config';
@@ -8,7 +8,7 @@ import GradientText from '../colors/general';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-function ImageBox(){
+function ImageBox() {
   const [image, setImage] = useState(null);
 
   useEffect(() => {
@@ -54,10 +54,18 @@ function PostScreen() {
   const genderButtons = ['Male', 'Female']
 
   return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <Button buttonStyle={{width: config.deviceWidth*0.4, marginLeft: config.deviceWidth*0.05}}></Button>
-        <Button buttonStyle={{width: config.deviceWidth*0.4, marginLeft: config.deviceWidth*0.05}}></Button>
+    <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <Button
+          buttonStyle={{ width: config.deviceWidth * 0.4, marginLeft: config.deviceWidth * 0.05, borderRadius: 99 }}
+          title='Post animal'
+          titleStyle={{ fontSize: 12 }}
+        />
+        <Button
+          buttonStyle={{ width: config.deviceWidth * 0.4, marginLeft: config.deviceWidth * 0.1, borderRadius: 99 }}
+          title='Cancel'
+          titleStyle={{ fontSize: 12 }}
+        />
       </View>
       <GradientText style={styles.chipText}>Images</GradientText>
       <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
@@ -68,10 +76,18 @@ function PostScreen() {
         <ImageBox />
         <ImageBox />
       </View>
+      <GradientText style={styles.chipText}>Gender</GradientText>
       <ButtonGroup
         buttons={genderButtons}
       />
-    </View>
+      <GradientText style={styles.chipText}>Date of Birth</GradientText>
+      <GradientText style={styles.chipText}>Name</GradientText>
+      <GradientText style={styles.chipText}>Breed</GradientText>
+      <GradientText style={styles.chipText}>Description</GradientText>
+      <GradientText style={styles.chipText}>Location</GradientText>
+      <GradientText style={styles.chipText}>Measurements</GradientText>
+      <GradientText style={styles.chipText}>Conditions</GradientText>
+    </ScrollView>
   );
 }
 
@@ -103,8 +119,12 @@ const styles = StyleSheet.create({
     fontStyle: 'normal',
     fontWeight: 'bold',
     fontSize: 25,
-    height: 35
+    height: 35,
   },
+  gradientButton: {
+    width: config.deviceWidth * 0.4,
+    marginLeft: config.deviceWidth * 0.05,
+  }
 })
 
 
