8958807f5bc3779dec27de071a8d34a0a68ddb05
Simon Jørgensen <sijo819@student.sdu.dk>
2021-10-27 20:06:12 +0200

    Add description
    Update post.js

--- components/views/post.js
+++ components/views/post.js
@@ -1,12 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { View, Image, Text, StyleSheet, Platform, ScrollView } from 'react-native';
-import { Button, ButtonGroup } from 'react-native-elements';
+import { Button, ButtonGroup, Input } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native';
 import config from '../config';
 import * as ImagePicker from 'expo-image-picker';
 import GradientText from '../colors/general';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { TextInput } from 'react-native';
+
+
+
+const conditions = [
+  // this is the parent or 'item'
+  {
+    name: 'Fruits',
+    id: 0,
+    // these are the children or 'sub items'
+    children: [
+      {
+        name: 'Apple',
+        id: 10,
+      },
+      {
+        name: 'Strawberry',
+        id: 17,
+      },
+      {
+        name: 'Pineapple',
+        id: 13,
+      },
+      {
+        name: 'Banana',
+        id: 14,
+      },
+      {
+        name: 'Watermelon',
+        id: 15,
+      },
+      {
+        name: 'Kiwi fruit',
+        id: 16,
+      },
+    ],
+  },
+];
 
 function ImageBox() {
   const [image, setImage] = useState(null);
@@ -82,10 +120,49 @@ function PostScreen() {
       />
       <GradientText style={styles.chipText}>Date of Birth</GradientText>
       <GradientText style={styles.chipText}>Name</GradientText>
+      <Input></Input>
       <GradientText style={styles.chipText}>Breed</GradientText>
+
       <GradientText style={styles.chipText}>Description</GradientText>
+      <TextInput
+        editable={true}
+        maxLength={200}
+        multiline={true}
+        numberOfLines={4}
+        placeholder='Write something nice about the animal...'
+        style={{ height: config.deviceWidth * 0.3, marginHorizontal: config.deviceWidth * 0.05, marginTop: 5, backgroundColor: 'rgba(234, 234, 234, 1)', borderRadius: 10, textAlignVertical: 'top', padding: 5 }}
+      />
+
       <GradientText style={styles.chipText}>Location</GradientText>
+      <View style={{ flex: 1, flexDirection: 'row', marginHorizontal: config.deviceWidth * 0.05 }}>
+        <TextInput style={{ width: config.deviceWidth * 0.725, backgroundColor: 'rgba(234, 234, 234, 1)', borderRadius: 8}}
+          placeholder={'Enter the location of your animal'}
+        ></TextInput>
+        <Button
+          buttonStyle={{width: config.deviceWidth *0.1125, height: config.deviceWidth*0.1, marginLeft: config.deviceWidth * 0.05}}
+        />
+      </View>
       <GradientText style={styles.chipText}>Measurements</GradientText>
+      <View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <Text>Weight</Text>
+          <TextInput style={{backgroundColor: 'rgba(234, 234, 234, 1)'}}
+          placeholder={'Enter weight in kg'}
+          ></TextInput>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <Text>Height</Text>
+          <TextInput style={{backgroundColor: 'rgba(234, 234, 234, 1)'}}
+          placeholder={'Enter height in kg'}
+          ></TextInput>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <Text>Length</Text>
+          <TextInput style={{backgroundColor: 'rgba(234, 234, 234, 1)'}}
+          placeholder={'Enter length in kg'}
+          ></TextInput>
+        </View>
+      </View>
       <GradientText style={styles.chipText}>Conditions</GradientText>
     </ScrollView>
   );
