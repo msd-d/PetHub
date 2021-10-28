09a5da1c995815c86abc3b213ff4da1b479fa8fc
Simon Jørgensen <sijo819@student.sdu.dk>
2021-10-28 11:08:26 +0200

    Update location.js

--- components/location.js
+++ components/location.js
@@ -1,23 +1,4 @@
-import React from 'react';
-import MapPicker from "react-native-map-picker";
-import {View} from "react-native";
+import React from 'react'
 
 export default class LocationPick extends React.Component {
-    state = {
-
-    };
-
-    render() {
-      return(
-        <View style={{flex: 1, width: '100%'}}>
-          <MapPicker
-            initialCoordinate={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-            }}
-            onLocationSelect={({latitude, longitude})=>console.log(longitude)}
-          />
-        </View>
-      );
-    }
 }
