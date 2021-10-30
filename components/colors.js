5f020f3d641f166691b2cda91d87d0e6f42664a4
Simon Jørgensen <sijo819@student.sdu.dk>
2021-10-30 19:34:30 +0200

    Add colors.js

---
+++ components/colors.js
@@ -0,0 +1,12 @@
+export default {
+    white: 'rgba(255, 255, 255, 1)',
+    black: 'rgba(0, 0, 0, 1)',
+    lightGray: 'rgba(234, 234, 234, 1)',
+    darkGray: 'rgba(76, 80, 85, 1)',
+    gray: 'rgba(139, 144, 147, 1)',
+    gradientColor1: 'rgba(223, 122, 153, 1)',
+    gradientColor2: 'rgba(232, 193, 171, 1)',
+}
+
+
+
--- components/colors/home-colors.js
+++
@@ -1,9 +0,0 @@
-const textColors = {
-    cardName: 'rgba(255, 255, 255, 1)',
-}
-
-const chipColors = {
-    chipBackground: 'rgba(255, 255, 255, 1)',
-}
-
-export { textColors, chipColors};
