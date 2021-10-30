544929a60a2f3ebf73371b0717dfbecef5985a24
Simon Jørgensen <sijo819@student.sdu.dk>
2021-10-30 19:46:22 +0200

    Update colors.js

--- components/colors.js
+++ components/colors.js
@@ -4,8 +4,8 @@ export default {
     lightGray: 'rgba(234, 234, 234, 1)',
     darkGray: 'rgba(76, 80, 85, 1)',
     gray: 'rgba(139, 144, 147, 1)',
-    gradientColor1: 'rgba(223, 122, 153, 1)',
-    gradientColor2: 'rgba(232, 193, 171, 1)',
+    pethubPink: 'rgba(223, 122, 153, 1)',
+    pethubLemon: 'rgba(232, 193, 171, 1)',
 }
 
 
