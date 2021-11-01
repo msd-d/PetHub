019eff40f24c631f8cac1dda6c7c74456d808d72
Simon Jørgensen <sijo819@student.sdu.dk>
2021-11-01 21:55:44 +0100

    Create database.js

---
+++ components/database.js
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { breeds, conditions, homeData } from "./dummy-data"
+
+
+
+export default class Database {
+
+    static setup() {
+        this.setItem(conditions, 'conditions');
+        this.setItem(breeds, 'breeds');
+        this.setItem(homeData, 'home');
+    }
+
+    static async getItem(key) {
+        try {
+            const jsonValue = await AsyncStorage.getItem(key)
+            return jsonValue != null ? JSON.parse(jsonValue) : null
+        } catch (e) {
+            // read error
+        }
+    }
+
+    static async setItem(value, key) {
+        try {
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem(key, jsonValue)
+        } catch (e) {
+            // save error
+        }
+    }
+}
