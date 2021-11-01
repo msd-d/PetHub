2f7693edf62a3e1b7fa150b18341d7c50357ea5d
Simon Jørgensen <sijo819@student.sdu.dk>
2021-11-01 19:17:43 +0100

    Update home.js

--- components/views/home.js
+++ components/views/home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Text, View, Image, FlatList, StyleSheet } from "react-native";
+import { Text, View, Image, FlatList } from "react-native";
 import { Chip } from "react-native-elements";
-import config from "../config";
 import GradientText from "../colors/gradient-text";
-import colors from "../colors";
+import homeStyle from "../styles/home-style";
 
 const apiData = [
   {
@@ -33,103 +32,52 @@ const apiData = [
 ];
 
 const renderItem = ({ item }) => (
-  <View style={styles.card}>
+  <View style={homeStyle.card}>
     <Image
       source={item.image}
       resizeMode="cover"
       overflow="hidden"
-      style={styles.image}
+      style={homeStyle.image}
     />
-    <View style={styles.cardContent}>
-      <Text style={styles.name}>{item.name}</Text>
-      <View style={styles.chipBox}>
+    <View style={homeStyle.cardContent}>
+      <Text style={homeStyle.name}>{item.name}</Text>
+      <View style={homeStyle.chipBox}>
         <Chip
           title={
-            <GradientText style={styles.chipText}>{item.breed}</GradientText>
+            <GradientText style={homeStyle.chipText}>{item.breed}</GradientText>
           }
-          titleStyle={styles.chipText}
-          buttonStyle={styles.chip}
+          titleStyle={homeStyle.chipText}
+          buttonStyle={homeStyle.chip}
         />
         <Chip
           title={
-            <GradientText style={styles.chipText}>{item.gender}</GradientText>
+            <GradientText style={homeStyle.chipText}>{item.gender}</GradientText>
           }
-          titleStyle={styles.chipText}
-          buttonStyle={styles.chip}
+          titleStyle={homeStyle.chipText}
+          buttonStyle={homeStyle.chip}
         />
         <Chip
           title={
-            <GradientText style={styles.chipText}>
+            <GradientText style={homeStyle.chipText}>
               {item.age + " years old"}
             </GradientText>
           }
-          titleStyle={styles.chipText}
-          buttonStyle={styles.chip}
+          titleStyle={homeStyle.chipText}
+          buttonStyle={homeStyle.chip}
         />
       </View>
     </View>
   </View>
 );
 
-function HomeScreen() {
-  return (
-    <FlatList
-      data={apiData}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={renderItem}
-    />
-  );
+export default class HomeScreen extends React.Component {
+  Render() {
+    return (
+      <FlatList
+        data={apiData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+      />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  card: {
-    flex: 1,
-    display: "flex",
-    width: config.deviceWidth * 0.9,
-    marginLeft: config.deviceWidth * 0.05,
-    height: config.deviceWidth * 0.9,
-    marginTop: 15,
-    borderRadius: 23,
-  },
-  cardContent: {
-    display: "flex",
-    flexDirection: "column",
-    position: "absolute",
-    bottom: 1,
-    alignSelf: "center",
-    width: config.deviceWidth * 0.8,
-    height: 100,
-  },
-  name: {
-    fontFamily: "Inter_700Bold",
-    fontStyle: "normal",
-    fontWeight: "bold",
-    fontSize: 22,
-    color: colors.white,
-    marginBottom: 10,
-  },
-  chip: {
-    backgroundColor: colors.white,
-    marginRight: 10,
-    borderRadius: 8,
-  },
-  chipText: {
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: "bold",
-    fontSize: 12,
-    height: 20,
-  },
-  chipBox: {
-    display: "flex",
-    flexDirection: "row",
-  },
-  image: {
-    width: undefined,
-    height: "100%",
-    overflow: "hidden",
-    borderRadius: 23,
-  },
-});
-
-export default HomeScreen;
