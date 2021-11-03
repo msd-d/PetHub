e93b04bd62175a0133a6d0b71b54a66893aca178
kepet19 <kevinmeejach@gmail.com>
2021-11-03 10:48:24 +0100

    Intial search mostly copied from Home

--- components/views/search.js
+++ components/views/search.js
@@ -1,13 +1,71 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { Standard } from "components/styles/temp-standard-style";
+import { Text, View, Image, FlatList } from "react-native";
+import { Chip } from "react-native-elements";
+import GradientText from "../colors/gradient-text";
+import Database from "../database";
+import searchStyle from "../styles/search-style";
 
-function SearchScreen() {
-  return (
-    <View style={Standard.container}>
-      <Text>Settings!</Text>
+const renderItem = ({ item }) => (
+  <View style={searchStyle.card}>
+    <Image
+      source={item.image}
+      resizeMode="cover"
+      overflow="hidden"
+      style={searchStyle.image}
+    />
+    <View style={searchStyle.cardContent}>
+      <Text style={searchStyle.name}>{item.name}</Text>
+      <View style={searchStyle.chipBox}>
+        <Chip
+          title={
+            <GradientText style={searchStyle.chipText}>{item.breed}</GradientText>
+          }
+          titleStyle={searchStyle.chipText}
+          buttonStyle={searchStyle.chip}
+        />
+        <Chip
+          title={
+            <GradientText style={searchStyle.chipText}>
+              {item.gender}
+            </GradientText>
+          }
+          titleStyle={searchStyle.chipText}
+          buttonStyle={searchStyle.chip}
+        />
+        <Chip
+          title={
+            <GradientText style={searchStyle.chipText}>
+              {item.age + " years old"}
+            </GradientText>
+          }
+          titleStyle={searchStyle.chipText}
+          buttonStyle={searchStyle.chip}
+        />
+      </View>
     </View>
-  );
-}
+  </View>
+);
 
-export default SearchScreen;
+export default class SearchScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      homeData: [],
+    };
+  }
+
+  componentDidMount() {
+    Database.getItem("home").then((data) => this.setState({ homeData: data }));
+  }
+
+  render() {
+    return (
+      <FlatList
+        data={this.state.homeData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        extraData={this.state.homeData}
+      />
+    );
+  }
+}
