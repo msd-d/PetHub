4ab3fef85af1d7745a44f25c1abb4759bfb890d6
Kechr <55458479+Kechr@users.noreply.github.com>
2021-11-06 11:55:01 +0100

    Change initial layout

--- components/views/login.js
+++ components/views/login.js
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { Standard } from "components/styles/save-style";
-import { Button } from "react-native-elements/dist/buttons/Button";
-import { Input } from "react-native-elements/dist/input/Input";
 import colors from "../colors";
 import loginStyles from "../styles/login-styles";
+import GradientButton from "../colors/gradient-button";
 
 class LoginScreen extends Component {
   render() {
     return (
       <View style={Standard.container}>
-        <Text style={loginStyles.header}>Login!</Text>
-        <Input></Input>
-        <Input style={{ color: colors.gray }}></Input>
-        <Button
-          style={{ backgroundColor: colors.pethubPink }}
-          title="Don't already have an account?"
+        <Text style={loginStyles.header}>Welcome!</Text>
+
+        <TextInput style={loginStyles.input} placeholder="Email" ></TextInput>
+        <TextInput style={loginStyles.input} placeholder="Password" ></TextInput>
+
+        <GradientButton
+          title={"Sign in"}
+          style={loginStyles.loginButton}
           onPress={() => alert("Not yet supported")}
-        ></Button>
+        ></GradientButton>
+
+        <Pressable 
+          onPress={() => alert("Not yet supported")}
+        >
+          <Text style= {{ margin: 10, color: colors.pethubPink }}>
+            Don't have an account yet?
+          </Text>
+        </Pressable>
+        <Text style= {{color: colors.pethubPink }}>or</Text>
+        <Pressable 
+          onPress={() => alert("Not yet supported")}
+        >
+          <Text style= {{ margin: 10, color: colors.pethubPink }}>
+            Continue as guest
+          </Text>
+        </Pressable>
       </View>
     );
   }
