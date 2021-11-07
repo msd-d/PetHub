7e7c38230c381a3badc0af660f53b99f87badd4e
Kechr <55458479+Kechr@users.noreply.github.com>
2021-11-07 12:40:32 +0100

    Inital profile layout

--- components/styles/profile-style.js
+++ components/styles/profile-style.js
@@ -1,11 +1,21 @@
 import { StyleSheet } from "react-native";
+import colors from "../colors";
+import config from "../config";
 
-const Standard = StyleSheet.create({
+export default StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
+  top: {
+    backgroundColor: colors.darkGray,
+    width: config.deviceWidth,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    borderRadius: 99,
+  },
 });
-
-export { Standard };
--- components/views/profile.js
+++ components/views/profile.js
@@ -1,9 +1,15 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, ViewPropTypes } from "react-native";
 import { Standard } from "components/styles/profile-style";
 import PropTypes from "prop-types";
 import AppContext from "../AppContext";
 import Database from "../database";
+import profileStyle from "../styles/profile-style";
+import { Button } from "react-native-elements/dist/buttons/Button";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import colors from "../colors";
+
 
 const ProfileScreen = () => {
   const [phone, setPhone] = useState("");
@@ -19,17 +25,48 @@ const ProfileScreen = () => {
       setEmail(user.email);
       setLocation(user.location);
       // TODO: Handle postings
-    })
-  })
+    });
+  });
 
   return (
-    <View style={Standard.container}>
+    <View style={profileStyle.container}>
       
+      {/* Settings, user photo, and logout */}
+      <View style={profileStyle.top}>
+        <Button
+          title={<Ionicons name={"settings"} size={40} color={colors.pethubPink}/>}
+          style={profileStyle.button}
+          onPress={() => alert("Not yet supported")}
+        />
+        <View>
+          <Ionicons name={"person"} size={100} color={colors.pethubPink}/>
+          <Text>{myContext.userID} </Text>
+        </View>
+          
+        <Button
+          title={<Ionicons name={"log-out"} size={40} color={colors.pethubPink}/>}
+          style={profileStyle.button}
+          onPress={() => alert("Not yet supported")}
+        />
+      </View>
 
-      <Text>{myContext.userID} </Text>
-      <Text>{phone} </Text>
-      <Text>{email} </Text>
-      <Text>{location} </Text>
+      {/* User info */}
+      <View>
+        <View>
+          <Ionicons name={"call"} size={100} color={colors.darkGray}/>
+          <Text>{phone} </Text>
+        </View>
+        <View>
+          <Ionicons name={"mail"} size={100} color={colors.darkGray}/>
+          <Text>{email} </Text>
+        </View>
+        <View>
+          <Ionicons name={"pin"} size={100} color={colors.darkGray}/>
+          <Text>{location} </Text>
+        </View>
+      </View>
+
+
     </View>
   );
 }
