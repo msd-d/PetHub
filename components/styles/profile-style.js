f8ff8241e80da2c9bada2fcd4a435578611c2755
Kechr <55458479+Kechr@users.noreply.github.com>
2021-11-11 09:47:33 +0100

    Style postings

--- components/styles/profile-style.js
+++ components/styles/profile-style.js
@@ -46,4 +46,68 @@ export default StyleSheet.create({
   button: {
     borderRadius: 99,
   },
+  // TODO: refactor this, right now it's duplicated in three files (home, search and here)
+  card: {
+    flex: 1,
+    display: "flex",
+    width: config.deviceWidth * 0.4,
+    marginLeft: config.deviceWidth * 0.05,
+    height: config.deviceWidth * 0.4,
+    marginTop: 15,
+    borderRadius: 23,
+  },
+  empty: {
+    flex: 1,
+    display: "flex",
+    width: config.deviceWidth * 0.4,
+    marginLeft: config.deviceWidth * 0.05,
+    height: config.deviceWidth * 0.4,
+    marginTop: 15,
+    borderRadius: 23,
+    backgroundColor: colors.lightGray,
+  },
+  star: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+  },
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    bottom: 1,
+    alignSelf: "center",
+    width: config.deviceWidth * 0.35,
+    height: 70,
+  },
+  name: {
+    fontFamily: "Inter_700Bold",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 22,
+    color: colors.white,
+    marginBottom: 5,
+  },
+  chip: {
+    backgroundColor: colors.white,
+    marginRight: 5,
+    borderRadius: 8,
+  },
+  chipText: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 6,
+    height: 10,
+  },
+  chipBox: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  image: {
+    width: undefined,
+    height: "100%",
+    overflow: "hidden",
+    borderRadius: 23,
+  },
 });
--- components/views/profile.js
+++ components/views/profile.js
@@ -8,14 +8,13 @@ import Database from "../database";
 import profileStyle from "../styles/profile-style";
 import { Button } from "react-native-elements/dist/buttons/Button";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import homeStyle from "../styles/home-style";
 import { getAgeFromTime } from "../util";
 
 import colors from "../colors";
 import { FlatList } from "react-native-gesture-handler";
 
 
-const ProfileScreen = () => {
+const ProfileScreen = ({ navigation }) => {
   const [isFetching, setIsFetching] = useState(false);
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
@@ -24,12 +23,14 @@ const ProfileScreen = () => {
 
   const myContext = useContext(AppContext);
 
+  // TODO: Fix this
   useEffect(() => {
     Database.getUser(myContext.userID).then((user) => {
       setPhone(user.phone);
       setEmail(user.email);
       setLocation(user.location);
       getData();
+      console.log("useEffect called")
     }), [];
   });
 
@@ -48,98 +49,71 @@ const ProfileScreen = () => {
     getData();
     setIsFetching(false);
   };
+
+  const logOut = () => {
+    myContext.updateUserID(null);
+    setPhone("");
+    setEmail("");
+    setLocation("");
+    setPostings([]);
+    navigation.navigate("Home");
+  }
   
+  // TODO: Make items smaller (1/2 screen size) and flex-wrap
   const renderItem = ({ item }) => (
-    <View style={homeStyle.card}>
+    <View style={profileStyle.card}>
       <Image
         source={{ uri: item.images[0] }}
         resizeMode="cover"
         overflow="hidden"
-        style={homeStyle.image}
+        style={profileStyle.image}
       />
-      <View style={homeStyle.cardContent}>
-        <Text style={homeStyle.name}>{item.name}</Text>
-        <View style={homeStyle.chipBox}>
+      <View style={profileStyle.cardContent}>
+        <Text style={profileStyle.name}>{item.name}</Text>
+        <View style={profileStyle.chipBox}>
           <Chip
             title={
-              <GradientText style={homeStyle.chipText}>
+              <GradientText style={profileStyle.chipText}>
                 {item.breeds.length > 1 ? "Mixed" : item.breeds[0]}
               </GradientText>
             }
-            titleStyle={homeStyle.chipText}
-            buttonStyle={homeStyle.chip}
+            titleStyle={profileStyle.chipText}
+            buttonStyle={profileStyle.chip}
           />
           <Chip
             title={
-              <GradientText style={homeStyle.chipText}>
+              <GradientText style={profileStyle.chipText}>
                 {item.gender}
               </GradientText>
             }
-            titleStyle={homeStyle.chipText}
-            buttonStyle={homeStyle.chip}
+            titleStyle={profileStyle.chipText}
+            buttonStyle={profileStyle.chip}
           />
           <Chip
             title={
-              <GradientText style={homeStyle.chipText}>
+              <GradientText style={profileStyle.chipText}>
                 {getAgeFromTime(new Date(item.birthDate)) + " y/o"}
               </GradientText>
             }
-            titleStyle={homeStyle.chipText}
-            buttonStyle={homeStyle.chip}
+            titleStyle={profileStyle.chipText}
+            buttonStyle={profileStyle.chip}
           />
         </View>
       </View>
     </View>
   );
 
-  const header = () => {
-    <View>
-      // Settings, user photo, and logout
-      <View style={profileStyle.top}>
-        <Button
-          title={<Ionicons name={"settings-outline"} size={40} color={colors.pethubPink}/>}
-          style={profileStyle.button}
-          onPress={() => alert("Not yet supported")}
-        />
-        <View style={profileStyle.profilePicture}>
-          <Ionicons name={"person"} size={100} color={colors.pethubPink}/>
-          <Text style={profileStyle.usernameText}>{myContext.userID}</Text>
-        </View>
-          
-        <Button
-          title={<Ionicons name={"log-out-outline"} size={40} color={colors.pethubPink}/>}
-          style={profileStyle.button}
-          onPress={() => alert("Not yet supported")}
-        />
-      </View>
-    
-      // User info
-      <View style={profileStyle.infoView}>
-        <View style={profileStyle.row}>
-          <Ionicons name={"call"} size={30} color={colors.darkGray}/>
-          <Text style={profileStyle.infoText}> {phone} </Text>
-        </View>
-        <View style={profileStyle.row}>
-          <Ionicons name={"at"} size={30} color={colors.darkGray}/>
-          <Text style={profileStyle.infoText}>{email} </Text>
-        </View>
-        <View style={profileStyle.row}>
-          <Ionicons name={"pin"} size={30} color={colors.darkGray}/>
-          <Text style={profileStyle.infoText}>{location} </Text>
-        </View>
-      </View>
-    </View>
-  };
-
   return (
-    <View>
-                // Settings, user photo, and logout
+    <FlatList
+        ListHeaderComponent={
+        <View>
+          {/* Settings, user photo, and logout */}
           <View style={profileStyle.top}>
             <Button
               title={<Ionicons name={"settings-outline"} size={40} color={colors.pethubPink}/>}
               style={profileStyle.button}
               onPress={() => alert("Not yet supported")}
-              />
+            />
             <View style={profileStyle.profilePicture}>
               <Ionicons name={"person"} size={100} color={colors.pethubPink}/>
               <Text style={profileStyle.usernameText}>{myContext.userID}</Text>
@@ -148,11 +122,13 @@ const ProfileScreen = () => {
             <Button
               title={<Ionicons name={"log-out-outline"} size={40} color={colors.pethubPink}/>}
               style={profileStyle.button}
-              onPress={() => alert("Not yet supported")}
-              />
+              onPress={() => logOut()}
+            />
           </View>
         
-          // User info
+          {/* User info 
+          *   TODO: Make component
+          */}
           <View style={profileStyle.infoView}>
             <View style={profileStyle.row}>
               <Ionicons name={"call"} size={30} color={colors.darkGray}/>
@@ -167,16 +143,15 @@ const ProfileScreen = () => {
               <Text style={profileStyle.infoText}>{location} </Text>
             </View>
           </View>
-
-      <FlatList 
-        data={postings}
-        extraData={postings}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={renderItem}
-        refreshing={isFetching}
-        onRefresh={() => onRefresh}
-        />
-      </View>
+        </View>
+      }
+      data={postings}
+      extraData={postings}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={renderItem}
+      refreshing={isFetching}
+      onRefresh={() => onRefresh}
+    />
   );
 }
 
