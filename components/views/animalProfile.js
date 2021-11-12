b6df9d261e69e76cbad5bf222c3ad5799588c4a5
SnaBe <fynboen1999@gmail.com>
2021-11-12 14:06:19 +0100

    Ran format

--- components/views/animalProfile.js
+++ components/views/animalProfile.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Text, View, Image, ScrollView, SafeAreaView, Linking, Alert } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  SafeAreaView,
+  Linking,
+  Alert,
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { LinearGradient } from "expo-linear-gradient";
 import Carousel from "react-native-snap-carousel";
@@ -20,25 +28,28 @@ const AnimalScreen = ({ route }) => {
       <ScrollView style={styles.scrollView}>
         <View style={generalStyles.flexRowMarginTop}>
           <View>
-            <Text style={styles.name}>{item.postedBy}</Text> 
+            <Text style={styles.name}>{item.postedBy}</Text>
             <View style={generalStyles.flexRow}>
               <Ionicons name={"location"} size={18} color={colors.gray} />
               <Text style={styles.locationName}>{item.location}</Text>
             </View>
-          </View> 
+          </View>
 
           <View style={styles.actions}>
-            <Button 
-              buttonStyle={styles.takeAction} 
+            <Button
+              buttonStyle={styles.takeAction}
               onPress={async () => {
                 const user = await Database.getUser(item.postedBy);
 
-                if(user !== undefined || user !== null) {
-                  if(user.email.length >= 4) {
+                if (user !== undefined || user !== null) {
+                  if (user.email.length >= 4) {
                     return Linking.openURL(`mailto:+${user.email}`);
                   }
 
-                  return Alert.alert("Error", "This user has not added a valid e-mail address.");
+                  return Alert.alert(
+                    "Error",
+                    "This user has not added a valid e-mail address."
+                  );
                 }
               }}
               ViewComponent={LinearGradient}
@@ -49,20 +60,23 @@ const AnimalScreen = ({ route }) => {
               }}
               icon={
                 <Ionicons name={"at-circle"} size={36} color={colors.white} />
-              }>
-            </Button>
-            
+              }
+            ></Button>
+
             <Button
-              buttonStyle={styles.takeAction} 
+              buttonStyle={styles.takeAction}
               onPress={async () => {
                 const user = await Database.getUser(item.postedBy);
 
-                if(user !== undefined || user !== null) {
-                  if(user.phone.length >= 8 && user.phone.length < 12) {
+                if (user !== undefined || user !== null) {
+                  if (user.phone.length >= 8 && user.phone.length < 12) {
                     return Linking.openURL(`tel:+45${user.phone}`);
                   }
 
-                  return Alert.alert("Error", "This user has not added their phone number.");
+                  return Alert.alert(
+                    "Error",
+                    "This user has not added their phone number."
+                  );
                 }
               }}
               ViewComponent={LinearGradient}
@@ -71,27 +85,29 @@ const AnimalScreen = ({ route }) => {
                 start: { x: -1, y: -1 },
                 end: { x: 1, y: 3 },
               }}
-              icon={
-                <Ionicons name={"call"} size={36} color={colors.white} />
-              }>
-            </Button>
+              icon={<Ionicons name={"call"} size={36} color={colors.white} />}
+            ></Button>
           </View>
-
         </View>
         <MyCarousel entries={item.images} />
         <Text style={styles.name}>{item.name}</Text>
 
         <View style={generalStyles.flexRow}>
           <GradientText style={styles.subItem}>{item.gender}</GradientText>
-          
-          <GradientText style={styles.subItem}>{Math.round(Math.abs(new Date(item.birthDate) - Date.now()) / (1000 * 60 * 60 * 24)/365)}<Text> years old</Text></GradientText>
+
+          <GradientText style={styles.subItem}>
+            {Math.round(
+              Math.abs(new Date(item.birthDate) - Date.now()) /
+                (1000 * 60 * 60 * 24) /
+                365
+            )}
+            <Text> years old</Text>
+          </GradientText>
         </View>
 
         <Text style={styles.description}>{item.description}</Text>
 
-        <View>
-
-        </View>
+        <View></View>
       </ScrollView>
     </SafeAreaView>
   );
