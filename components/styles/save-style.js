455c090035f2de05b6d9210991f64d2089374a24
Bokaiii <55395900+Bokaiii@users.noreply.github.com>
2021-11-14 13:35:29 +0100

    Adds save property to data + access from save-view (#28)
    * Add saved property to data + access from saved-view
    
    * Add basic remove button w/o functionality
    
    * Make remove button use id and position dynamic
    
    * Remove boolean saved property from dummy-data
    
    * Fix saved view to display saved for specific user
    
    * Implement remove function in saved view
    
    * Add PropTypes
    
    * Rename userID to username in PropTypes
    
    Co-authored-by: Simon Jørgensen <sijo819@student.sdu.dk>

--- components/styles/save-style.js
+++ components/styles/save-style.js
@@ -1,11 +1,85 @@
 import { StyleSheet } from "react-native";
+import config from "../config";
+import colors from "../colors";
 
-const Standard = StyleSheet.create({
+export default StyleSheet.create({
+  card: {
+    flex: 1,
+    display: "flex",
+    width: config.deviceWidth * 0.9,
+    marginLeft: config.deviceWidth * 0.05,
+    height: config.deviceWidth * 0.9,
+    marginTop: 15,
+    borderRadius: 23,
+  },
+
+  image: {
+    width: undefined,
+    height: "100%",
+    overflow: "hidden",
+    borderRadius: 23,
+  },
+
+  remove: {
+    backgroundColor: colors.white,
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    borderRadius: 100,
+    display: "flex",
+    flexDirection: "row-reverse",
+  },
+
+  name: {
+    fontFamily: "Inter_700Bold",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 22,
+    color: colors.white,
+    marginBottom: 10,
+  },
+
+  chipBox: {
+    display: "flex",
+    flexDirection: "row",
+  },
+
+  chipText: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 12,
+    height: 20,
+  },
+
+  chip: {
+    backgroundColor: colors.white,
+    marginRight: 10,
+    borderRadius: 8,
+  },
+
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-});
 
-export { Standard };
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    bottom: 1,
+    alignSelf: "center",
+    width: config.deviceWidth * 0.8,
+    height: 100,
+  },
+
+  removeBox: {
+    display: "flex",
+    flexDirection: "row-reverse",
+    position: "absolute",
+    margin: config.deviceWidth * 0.05,
+    top: 1,
+    width: config.deviceWidth * 0.8,
+  },
+});
--- components/views/save.js
+++ components/views/save.js
@@ -1,13 +1,108 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { Standard } from "components/styles/save-style";
+import React, { useContext, useState, useEffect } from "react";
+import { Text, View, Image, FlatList, TouchableOpacity } from "react-native";
+import { Chip } from "react-native-elements/dist/buttons/Chip";
+import GradientText from "../colors/gradient-text";
+import Database from "../database";
+import saveStyle from "../styles/save-style";
+import AppContext from "../AppContext";
+import PropTypes from "prop-types";
 
-function SaveScreen() {
-  return (
-    <View style={Standard.container}>
-      <Text>Save!</Text>
+const RenderSaveScreen = ({ item, username }) => (
+  <View style={saveStyle.card}>
+    <Image
+      source={{ uri: item.images[0] }}
+      resizeMode="cover"
+      overflow="hidden"
+      style={saveStyle.image}
+    />
+    <View style={saveStyle.removeBox}>
+      <TouchableOpacity
+        style={saveStyle.remove}
+        onPress={() => {
+          Database.removeSaved(item.id, username);
+        }}
+      />
     </View>
+    <View style={saveStyle.cardContent}>
+      <Text style={saveStyle.name}>{item.name}</Text>
+
+      <View style={saveStyle.chipBox}>
+        <Chip
+          title={
+            <GradientText style={saveStyle.chipText}>
+              {item.breeds.length > 1 ? "Mixed" : item.breeds[0]}
+            </GradientText>
+          }
+          titleStyle={saveStyle.chipText}
+          buttonStyle={saveStyle.chip}
+        />
+        <Chip
+          title={
+            <GradientText style={saveStyle.chipText}>
+              {item.gender}
+            </GradientText>
+          }
+          titleStyle={saveStyle.chipText}
+          buttonStyle={saveStyle.chip}
+        />
+        <Chip
+          title={
+            <GradientText style={saveStyle.chipText}>
+              {new Date().getFullYear() - item.birthDate.year + " y/o"}
+            </GradientText>
+          }
+          titleStyle={saveStyle.chipText}
+          buttonStyle={saveStyle.chip}
+        />
+      </View>
+    </View>
+  </View>
+);
+
+RenderSaveScreen.propTypes = {
+  item: PropTypes.object.isRequired,
+  username: PropTypes.string.isRequired,
+};
+
+export default function SavedScreen() {
+  const myContext = useContext(AppContext);
+  const [savedData, setSavedData] = useState([]);
+  const [fetching, setFetching] = useState(false);
+
+  const RenderItem = ({ item }) => {
+    return <RenderSaveScreen item={item} username={myContext.userID} />;
+  };
+
+  RenderItem.propTypes = {
+    item: PropTypes.object.isRequired,
+  };
+
+  const getData = async () => {
+    const data = await Database.getItem("data");
+    const saved = await Database.getSaved(myContext.userID);
+
+    const filtered = data.filter((item) => saved.includes(item.id));
+    setSavedData(filtered);
+  };
+
+  const onRefresh = () => {
+    setFetching(true);
+    getData();
+    setFetching(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <FlatList
+      data={savedData}
+      extraData={savedData}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={RenderItem}
+      refreshing={fetching}
+      onRefresh={() => onRefresh()}
+    />
   );
 }
-
-export default SaveScreen;
